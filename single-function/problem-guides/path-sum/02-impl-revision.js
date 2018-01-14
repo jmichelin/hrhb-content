b61f50bfdfc3285209d1931aafd34414faba5599
JohnMichelin <jmichelinphone@gmail.com>
2018-01-13 17:06:23 -0800

    chore(sfp): move sfp back into sf directory

--- single-function/problem-guides/path-sum/02-impl-revision.js
+++
@@ -1,7 +0,0 @@
-const hasPathSum = function(node, targetSum) {
-  if (!node) return targetSum === 0;
-  targetSum -= node.val;
-  return hasPathSum(node.left, targetSum) || hasPathSum(node.right, targetSum);
-};
-
-module.exports = hasPathSum;
