7004812fb9308a96b1de530e297adafc7a4c4483
JohnMichelin <jmichelinphone@gmail.com>
2018-01-15 08:48:42 -0800

    Revert "feat:"

--- generateTemplat.sh
+++ generateTemplat.sh
@@ -10,10 +10,7 @@ if [ "$1" != "" ]; then
     mkdir $1
     cd $1
     echo "{ \"lesson_title\": \"$1\" }" >> lesson_settings.json
-    echo "## $1" >> lesson.md
-    echo "// Code Here" >> main.js
-    echo "// Tests Here" >> lesson_tests.js
 
 else
-    echo "You must supply a lesson name in the format of lesson-name"
+    echo "You must supply a lesson name in the format of ##-lesson-name"
 fi
