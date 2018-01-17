17c749ebbb38248f70fb1515d5eb66bea14f1c3c
tim <manzoid@gmail.com>
2018-01-17 15:39:35 -0800

    update reference solution for addIntegerLists()

--- single-function/add-integer-lists/02-impl-revision.js
+++ single-function/add-integer-lists/02-impl-revision.js
@@ -1,21 +1,19 @@
 const addIntegerLists = (listA, listB) => {
-  let carryOver = 0;
-  const dummyHead = { val: 0, next: null };
+  const dummyHead = { val: null, next: null };
   let curr = dummyHead;
+  let carry = 0;
 
-  while (listA || listB ) {
-    const listAVal = listA !== null ? listA.val : 0;
-    const listBVal = listB !== null ? listB.val : 0;
-    const sum = listAVal + listBVal + carryOver;
-    carryOver = Math.floor(sum / 10);
+  while (listA || listB || carry) {
+    const valA = listA ? listA.val : 0;
+    const valB = listB ? listB.val : 0;
+
+    const sum = valA + valB + carry;
+    carry = Math.floor(sum / 10);
     curr.next = { val: sum % 10, next: null };
-    curr = curr.next;
-    if (listA !== null) listA = listA.next;
-    if (listB !== null) listB = listB.next;
-  }
 
-  if (carryOver > 0) {
-    curr.next = { val: carryOver, next: null };
+    curr = curr.next;
+    if (listA) listA = listA.next;
+    if (listB) listB = listB.next;
   }
 
   return dummyHead.next;
