74a7b8c5e027f82c23ff5fd2513bc993642376d0
JohnMichelin <jmichelinphone@gmail.com>
2018-01-18 21:27:42 -0800

    Update longest-common-prefix.test.js

--- single-function/longest-common-prefix/longest-common-prefix.test.js
+++ single-function/longest-common-prefix/longest-common-prefix.test.js
@@ -1,30 +1,21 @@
 const longestCommonPrefix = require('./02-impl-revision');
 
-describe('addIntegerLists', () => {
+describe('longestCommonPrefix', () => {
   it('given empty array, [] return ""', () => {
-
     const arrayOfStrings = [];
-
     const expected = '';
-
     expect(longestCommonPrefix(arrayOfStrings)).toEqual(expected);
   });
 
   it('given array, ["aaabb", "aabb", "xaacc"] return ""', () => {
-
     const arrayOfStrings = ["aaabb", "aabb", "xaacc"];
-
     const expected = '';
-
     expect(longestCommonPrefix(arrayOfStrings)).toEqual(expected);
   });
 
   it('given array, ["aaabb", "aabb", "aacc"] return "aa"', () => {
-
     const arrayOfStrings = ["aaabb", "aabb", "aacc"];
-
     const expected = 'aa';
-
     expect(longestCommonPrefix(arrayOfStrings)).toEqual(expected);
   });
 });
