aaf7da61bb8fd55da820401a2a193485f8cdfcbc
tim <manzoid@gmail.com>
2018-01-20 02:04:26 -0800

    remove moved files to avoid gnarly merge conflict.

--- single-function/problem-guides/has-path-sum/02-impl-revision.js
+++
@@ -1,7 +0,0 @@
-const hasPathSum = function(node, targetSum) {
-  if (!node) return false;
-  if (!node.left && !node.right) return targetSum === node.val;
-  return hasPathSum(node.left, targetSum - node.val) || hasPathSum(node.right, targetSum - node.val);
-};
-
-module.exports = hasPathSum;
