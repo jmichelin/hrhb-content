1e9369c02aed8132cb55a19eb3ca0d36ed78bc1e
tim <manzoid@gmail.com>
2018-01-24 10:25:10 -0800

    tighten up setup and impl.

--- single-function/reverse-linked-list/reference-solution.js
+++ single-function/reverse-linked-list/reference-solution.js
@@ -1,21 +1,16 @@
 const reverseLinkedList = (head) => { // iterative
-  let cur = head;
-  let next;
-  let temporaryHead = null;
+  let previous = null;
+  let current = head;
+  let next = null;
 
-  while(cur) {
-    next = cur.next;
-    cur.next = temporaryHead;
-
-    if (next) {
-      temporaryHead = cur;
-      cur = next;
-    } else {
-      return cur;
-    }
+  while(current) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
   }
 
-  return null;
+  return previous;
 };
 
 module.exports = reverseLinkedList;
@@ -25,41 +20,37 @@ Input: { val: 1, next: { val: 2, next: { val: 3, next: null } } }
 Output: { val: 3, next: { val: 2, next: { val: 1, next: null  } } }
 
 Strategy
-  Create a place holder and pop each linked list node off starting at the head of the given linked
-  list and put it in as the head. Continue until the last item as been placed at the front of the
-  new list.
+Iterate each node and update its "next" pointer to point at the previous node.
 
-Given: { val: 1, next: { val: 2, next: { val: 3, next: null } } }
-Step 1: (1)->(2)->(3) = (1) and (2)->(3)
-Step 2: (2)->(1) and (3)
-Step 3: (3)->(2)->(1) and done
+Example
 
-const reverseLinkedList = (head) => { // iterative
-  // Set head as current and create a temporary head
+Given: 1 -> 2 -> 3 -> null
 
-  // Iterate through list until at the end
-    // save current nodes next value and point at the head
-    // if not at the end of the list
-      // set head to current and current to next
-   // else return linked list
+Loop 1:
+prev | curr | curr.next | next | note
+null | 1    | 2         | null | initial state
+null | 1    | 2         | 2    | update next
+null | 1    | null      | 2    | point curr.next at previous
+1    | 1    | null      | 2    | update previous
+1    | 2    | 3         | 2    | update current
+
+Loop 2:
+1    | 2    | 3         | 2 | initial state
+... repeat above steps...
+
+
+const reverseLinkedList = (head) => {
+  let previous = null;
+  let current = head;
+  let next = null;
+
+  while(current) {
+    // update next
+    // update current to point at previous
+    // update previous to be current
+    // update current to be next
+  }
+
+  return previous;
 };
-
-//
-
-Step 2:
-  next = { val: 2, next: { val: 3, next: null } }
-  cur.next = { val: 1, next: null }
-  head = (1)
-  cur = { val: 2, next: { val: 3, next: null } }
-Step 3:
-  cur = { val: 2, next: { val: 3, next: null } }
-  next = { val: 3, next: null }
-  cur.next = { val: 1, next: null }
-  cur = { val: 2, next: { val: 1, next: null } }
-  head = { val: 2, next: { val: 1, next: null } }
-  cur = { val: 3, next: null }
-Step 4:
-  next = null
-  cur.next = { val: 2, next: { val: 1, next: null } }
-  return cur = { val: 3, next: { val: 2, next: { val: 1, next: null } } }
- */
+*/
--- single-function/reverse-linked-list/reverse-linked-list.test.js
+++ single-function/reverse-linked-list/reverse-linked-list.test.js
@@ -7,11 +7,15 @@ describe('reverseLinkedList', () => {
 
   it('should accept 1->2->3 and return 3->2->1', function () {
     const testList = { val: 1, next: { val: 2, next: { val: 3, next: null, }, },};
-
     const expected = { val: 3, next: { val: 2, next: { val: 1, next: null, }, },};
-
     expect(reverseLinkedList(testList)).toEqual(expected);
-
   });
+
+  it('should accept 1-> and return 1->', function () {
+    const testList = { val: 1, next: null};
+    const expected = { val: 1, next: null};
+    expect(reverseLinkedList(testList)).toEqual(expected);
+  });
+
 });
 
