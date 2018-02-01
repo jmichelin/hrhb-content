364bacc004d96939a882bcb87df20d7c58e5de60
JohnMichelin <jmichelinphone@gmail.com>
2018-02-01 10:42:23 -0800

    test(blocks): count blocks inbetween test

--- single-function/container-with-most-water/reference-solution.js
+++ single-function/container-with-most-water/reference-solution.js
@@ -2,12 +2,15 @@ const containerWithMostWater = (height) => {
   let leftWall = 0;
   let rightWall = height.length - 1;
   let water = 0;
+  let blocksBetween = 0;
   const containerMap = {
     heightMap: height,
     largestContainer: [],
   };
   while (leftWall < rightWall) {
     water = Math.max(water, (rightWall - leftWall)*Math.min(height[leftWall], height[rightWall]));
+    blocksBetween = height.slice((rightWall-1), (leftWall+1));
+    console.log(blocksBetween);
     containerMap.largestContainer = [leftWall, rightWall, water];
     (height[leftWall] < height[rightWall]) ? leftWall++ : rightWall--;
   }
